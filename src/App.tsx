7c4da408ba93e8bfcc6c67c5c595a577874b48c3
Damien <damien.dev2022@gmail.com>
2025-09-09 12:57:36 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeaderSection from "./components/HeaderSection";
+
+function App() {
+  return (
+    <main>
+      <HeaderSection />
+    </main>
+  );
+}
+
+export default App;
