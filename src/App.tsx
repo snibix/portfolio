d03668f92e4928df62336e78577ecaca5df7dc8d
Damien <damien.dev2022@gmail.com>
2025-09-09 13:17:25 +0200

    Add Component Skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeaderSection from "./components/HeaderSection";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <main>
       <HeaderSection />
+      <Skills />
     </main>
   );
 }
